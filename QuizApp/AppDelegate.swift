ce953e997c954f5454507708b062e4abc2224c06
Alex Stophel <alexstophel@gmail.com>
2016-07-20 20:46:45 -0500

    Initial commit.

---
+++ QuizApp/AppDelegate.swift
@@ -0,0 +1,21 @@
+//
+//  AppDelegate.swift
+//  QuizApp
+//
+//  Created by Alex Stophel on 7/17/16.
+//  Copyright © 2016 Alex Stophel. All rights reserved.
+//
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        // Override point for customization after application launch.
+        return true
+    }
+
+}
